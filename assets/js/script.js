fb511129b67a185ce62285b403f7108af8bc3c9e
KongSunHwan <tjsghks78@gmail.com>
2022-06-22 16:18:58 +0900

    Sass기반 나만의 포트폴리오 최종본

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+
+
+  // typing text animation script
+  var typed = new Typed(".typing", {
+    strings: ["개발자를 꿈꾸는", "학생", "웹프로그래머 입니다."],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+
+
+
+});
+filterSelection("all")
+function filterSelection(c) {
+  var x, i;
+  x = document.getElementsByClassName("filterDiv");
+  if (c == "all") c = "";
+  for (i = 0; i < x.length; i++) {
+    w3RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+  }
+}
+
+function w3AddClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) { element.className += " " + arr2[i]; }
+  }
+}
+
+function w3RemoveClass(element, name) {
+  var i, arr1, arr2;
+  arr1 = element.className.split(" ");
+  arr2 = name.split(" ");
+  for (i = 0; i < arr2.length; i++) {
+    while (arr1.indexOf(arr2[i]) > -1) {
+      arr1.splice(arr1.indexOf(arr2[i]), 1);
+    }
+  }
+  element.className = arr1.join(" ");
+}
+
+// Add active class to the current button (highlight it)
+var btnContainer = document.getElementById("myBtnContainer");
+var btns = btnContainer.getElementsByClassName("btn2");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    var current = document.getElementsByClassName("active");
+    current[2].className = current[2].className.replace(" active", "");
+    this.className += " active";
+  });
+}
+const login1 = document.getElementById('login');
+
+login1.addEventListener('click', () => {
+  location.href = "login.html";
+});
